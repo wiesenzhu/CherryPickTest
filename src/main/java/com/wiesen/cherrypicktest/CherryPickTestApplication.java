7dcc8e149ddf4eea2fd6424938df642463a80bfe
zhubing <zhubing@yunjiacloud.com>
2022-04-12 09:23:47 +0800

    init project

---
+++ src/main/java/com/wiesen/cherrypicktest/CherryPickTestApplication.java
@@ -0,0 +1,13 @@
+package com.wiesen.cherrypicktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryPickTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherryPickTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiesen/cherrypicktest/CherryPickTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiesen.cherrypicktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryPickTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
