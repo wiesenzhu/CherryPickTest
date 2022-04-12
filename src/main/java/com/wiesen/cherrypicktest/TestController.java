ca31c57ad632a58ef4957963cbf5484ead13ae70
zhubing <zhubing@yunjiacloud.com>
2022-04-12 09:26:01 +0800

    #add 增加控制器

---
+++ src/main/java/com/wiesen/cherrypicktest/TestController.java
@@ -0,0 +1,22 @@
+package com.wiesen.cherrypicktest;
+
+/**
+ * @author zhubing
+ * @date 2022/4/12 9:25
+ */
+public class TestController {
+
+
+    public String hello() {
+        return "hello";
+    }
+
+    public String hey() {
+        return "hey";
+    }
+
+    public String hi() {
+        return "hi";
+    }
+
+}
